0a6f2f357cf975f5df5bf1dd64093499bd528e89
FabriceMk <FabriceMk@users.noreply.github.com>
2017-04-04 23:38:04 +0900

    Fixes karma.conf.js for checking Travis env variable

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
 module.exports = function (config) {
-  config.set({
+  var configuration = {
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
@@ -43,9 +43,12 @@ module.exports = function (config) {
         flags: ['--no-sandbox']
       }
     }
-  });
+  };
+
+  if(process.env.TRAVIS) {  
+    configuration.browsers = ['Chrome_travis_ci'];
+  }
+
+  config.set(configuration);
 };
 
-if(process.env.TRAVIS) {  
-  configuration.browsers = ['Chrome_travis_ci'];
-}
