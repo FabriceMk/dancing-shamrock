edeee19604a385ad27f0149812a9749e6efc8908
FabriceMk <fabricemk@users.noreply.github.com>
2017-09-10 18:23:53 +0900

    Displays all data for a figure.

--- src/app/dances/set-dances-details/set-dances-details.component.ts
+++ src/app/dances/set-dances-details/set-dances-details.component.ts
@@ -5,6 +5,7 @@ import 'rxjs/add/operator/switchMap';
 
 import { DancesService } from '../shared/dances.service';
 import { SetDance } from '../shared/models/set-dance.model';
+import { SetDanceMovementSpecialType } from '../shared/set-dance-movement-special-type.enum';
 
 /**
  * Component which displays the details of a set dance.
@@ -18,7 +19,13 @@ export class SetDancesDetailsComponent implements OnInit {
   /** The set dance to display the details from. */
   dance: SetDance;
 
+  /** List of special movement types ids. */
+  specialType = SetDanceMovementSpecialType;
+
+  /** Loading state of the component data. */
   isLoading = true;
+
+  /** Error state of the component data. */
   hasError = false;
 
   constructor(
---
+++ src/app/dances/shared/set-dance-movement-special-type.enum.ts
@@ -0,0 +1,8 @@
+/**
+ * Enum containing the special ids for certain special types of
+ * set dance movements.
+ */
+export enum SetDanceMovementSpecialType {
+  REPEAT = 'repeat',
+  GROUP = 'group'
+}
