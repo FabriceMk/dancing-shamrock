7607504444d6494cf992a61c1d466964fa7cc3e8
FabriceMk <fabricemk@users.noreply.github.com>
2017-09-14 00:57:30 +0900

    Adds image and video information for each figure.

--- src/app/dances/shared/models/set-dance-figure.model.ts
+++ src/app/dances/shared/models/set-dance-figure.model.ts
@@ -13,6 +13,12 @@ export class SetDanceFigure {
   /** Duration in bars of the Figure */
   duration: number;
 
+  /** Link to a video tutorial of that Figure */
+  videoUrl: string;
+
+  /** Link to the video thumbnail */
+  videoThumbnailUrl: string;
+
   /** List of Movements in the Figure */
   movements: SetDanceMovement[];
 }
