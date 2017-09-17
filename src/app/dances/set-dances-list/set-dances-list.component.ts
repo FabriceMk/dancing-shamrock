0bb3773fcf6662926877630a84420f74f02ce3b5
FabriceMk <fabricemk@users.noreply.github.com>
2017-09-17 19:21:48 +0900

    Enables navigation from SetDancesList and adds back to Home button in the toolbar.

--- src/app/dances/set-dances-list/set-dances-list.component.ts
+++ src/app/dances/set-dances-list/set-dances-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { DancesService } from '../shared/dances.service';
 
@@ -16,18 +17,31 @@ export class SetDancesListComponent implements OnInit {
   errorMessage: string;
   dances: SetDanceOverview[];
 
-  constructor(private dancesService: DancesService) { }
+  constructor(
+    private router: Router,
+    private dancesService: DancesService) { }
 
   ngOnInit() {
     this.loadDances();
   }
 
+  /**
+   * Load the set dances list.
+   */
   loadDances(): void {
     this.dancesService.getDancesList()
       .subscribe(
         dances => this.dances = dances,
         error => this.errorMessage = <any>error
       );
+  }
+
+  /**
+   * Navigate to the dance details.
+   * @param danceId Id of the dance
+   */
+  navigateToDetails(danceId: string) {
+    this.router.navigate(['set-dances', 'clare-lancers']);
   }
 
   displaySteps(dance: SetDanceOverview) {
