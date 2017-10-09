adf0f3874916a261f1cc76a3af2776e45d2df498
FabriceMk <fabricemk@users.noreply.github.com>
2017-10-09 11:38:56 +0900

    First version of the About Dialog

---
+++ src/app/core/help/about-dialog.component.ts
@@ -0,0 +1,21 @@
+import {Component, Inject} from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+
+/**
+ * @title Dialog for 'About' information
+ */
+@Component({
+  selector: 'app-about-dialog',
+  templateUrl: './about-dialog.component.html',
+  styleUrls: ['./about-dialog.component.scss']
+})
+export class AboutDialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<AboutDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
